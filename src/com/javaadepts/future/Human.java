ced972d2ea10940744aa5f0acb3c938d75df1e9b
RinaTyo <moonrina@mail.ru>
2022-01-15 14:19:58 +0400

    new project added

---
+++ src/com/javaadepts/future/Human.java
@@ -0,0 +1,31 @@
+package com.javaadepts.future;
+
+public class Human {
+    private int age;
+    private String name;
+    private int legs;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+}
---
+++ src/com/javaadepts/future/Pivo.java
@@ -0,0 +1,31 @@
+package com.javaadepts.future;
+
+public class Pivo {
+    private String name;
+    private Integer cost;
+    private boolean sale;
+
+    public boolean isSale() {
+        return sale;
+    }
+
+    public void setSale(boolean sale) {
+        this.sale = sale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
